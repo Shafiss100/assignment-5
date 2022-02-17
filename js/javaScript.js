f33b252805186fe7908e0052d76e2dfe0759e8dd
Sheikh Shafi <shafiss01318020612@gmail.com>
2022-02-17 11:50:26 +0600

    secont step complete

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,6 +1,5 @@
 document.getElementById("calculate-btn").addEventListener("click", function(){
     const income = document.getElementById("income-input").value ;
-
     const foodCast = document.getElementById("food-input").value ;
     const rentCast = document.getElementById("rent-input").value ;
     const clothCast = document.getElementById("cloth-input").value ;
@@ -8,7 +7,17 @@ document.getElementById("calculate-btn").addEventListener("click", function(){
     var totalExpenses = document.getElementById("total-expenses");
     var balance = document.getElementById("balance");
     totalExpenses.innerText = total;
-    balance.innerText = parseFloat(income) - total ;
-
+    balance.innerText = parseFloat(income) - total;
+})
+document.getElementById("save-btn").addEventListener("click", function(){
+    const balance = document.getElementById("balance");
+    const income = document.getElementById("income-input").value;
+    const parsent = document.getElementById("parsent").value ;
+    const savingAmount = document.getElementById("saving-amount");
+    savingAmountValue = (parseFloat(income) * parseFloat(parsent))/100 ;
+    savingAmount.innerText = savingAmountValue;
+    const remainingBalance = document.getElementById("remaining-balance");
+    remainingBalanceValue = parseFloat(balance.innerText) - savingAmountValue;
+    remainingBalance.innerText = remainingBalanceValue;
 
 })
