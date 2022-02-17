6331b9aec28eaaba115d983cf549078e930a098f
Sheikh Shafi <shafiss01318020612@gmail.com>
2022-02-17 12:15:22 +0600

    error add

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,14 +1,32 @@
 document.getElementById("calculate-btn").addEventListener("click", function(){
+
     const income = document.getElementById("income-input").value ;
+
+
+    // expenses id input value 
     const foodCast = document.getElementById("food-input").value ;
     const rentCast = document.getElementById("rent-input").value ;
     const clothCast = document.getElementById("cloth-input").value ;
-    let total = parseFloat(foodCast) + parseFloat(rentCast) + parseFloat(clothCast);
-    var totalExpenses = document.getElementById("total-expenses");
-    var balance = document.getElementById("balance");
-    totalExpenses.innerText = total;
-    balance.innerText = parseFloat(income) - total;
+    if(0<parseFloat(foodCast) && 0<parseFloat(rentCast) && 0<parseFloat(clothCast)){
+        let total = parseFloat(foodCast) + parseFloat(rentCast) + parseFloat(clothCast);
+        if ( parseFloat(income) > total){
+            var totalExpenses = document.getElementById("total-expenses");
+            var balance = document.getElementById("balance");
+            totalExpenses.innerText = total;
+            balance.innerText = parseFloat(income) - total;
+        }
+        else{
+            document.getElementById("error").style.display = "block";
+        }
+    }
+    else{
+        document.getElementById("error").style.display = "block";
+    }
+    
+    
 })
+
+// parsent calculation --------------------------
 document.getElementById("save-btn").addEventListener("click", function(){
     const balance = document.getElementById("balance");
     const income = document.getElementById("income-input").value;
